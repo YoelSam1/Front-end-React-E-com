0907afc965a3017cc7755a118158f0b69bb2c8dc
Eamith <yoesam36962@stud.noroff.no>
2024-06-06 00:32:16 +0200

    update fix link contact page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,7 +18,7 @@ const NavigationBar = () => {
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
               <Nav.Link href="/">Home</Nav.Link>
-              <Nav.Link href="/contact">Contact</Nav.Link>
+              <Nav.Link href="../pages/contact.jsx">Contact</Nav.Link>
               <Nav.Link href="/about">About</Nav.Link>
             </Nav>
             <Nav.Link href="/cart">
