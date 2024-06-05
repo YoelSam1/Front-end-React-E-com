99ff70599aa4d5ee8af72859331a869a251170e1
Eamith <yoesam36962@stud.noroff.no>
2024-06-06 00:07:34 +0200

    update rest changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { fetchProducts } from "./components/fetch-api";
+import Home from "./pages/home";
+import Contact from "./pages/contact";
+import About from "./pages/about";
+import ProductDetail from "./pages/ProductDetail"; // Correct import
+import Navbar from "./components/Navbar"; // Keep the name as Navbar
+import "./App.css";
+import Cart from "./pages/cart";
+import { CartProvider } from "./context/CartContext"; // Import CartProvider
+import CheckoutSuccess from "./pages/CheckoutSuccess";
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const productsData = await fetchProducts();
+      setProducts(productsData);
+    };
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CartProvider>
+      <Router>
+        <Navbar />
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home products={products} />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/product/:id" element={<ProductDetail />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/checkout-success" element={<CheckoutSuccess />} />
+          </Routes>
+        </Layout>
+      </Router>
+    </CartProvider>
   );
 }
 
