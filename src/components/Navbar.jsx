8fc714ccf2493449206d9b42e9af9638eeae0791
Eamith <yoesam36962@stud.noroff.no>
2024-06-06 00:37:49 +0200

    fix issue about react-router-dom

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { FaShoppingCart } from "react-icons/fa";
 import { useCart } from "../context/CartContext";
+import { Link } from "react-router-dom"; // Import Link
 
 const NavigationBar = () => {
   const { cartCount } = useCart();
@@ -10,18 +11,24 @@ const NavigationBar = () => {
     <>
       <Navbar bg="light" expand="lg" fixed="top">
         <Container>
-          <Navbar.Brand href="/">
+          <Navbar.Brand as={Link} to="/">
             <strong>E-com</strong>
           </Navbar.Brand>
 
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="/">Home</Nav.Link>
-              <Nav.Link href="../pages/contact.jsx">Contact</Nav.Link>
-              <Nav.Link href="/about">About</Nav.Link>
+              <Nav.Link as={Link} to="/">
+                Home
+              </Nav.Link>
+              <Nav.Link as={Link} to="/contact">
+                Contact
+              </Nav.Link>
+              <Nav.Link as={Link} to="/about">
+                About
+              </Nav.Link>
             </Nav>
-            <Nav.Link href="/cart">
+            <Nav.Link as={Link} to="/cart">
               <FaShoppingCart /> {cartCount}
             </Nav.Link>
           </Navbar.Collapse>
